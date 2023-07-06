03c35e42f8b468e4da5e686c5ed5c96c4012271a
Noah Tsegay <noahtsegay8@gmail.com>
2023-07-06 23:00:09 +0300

    Update 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+def canUnlockAll(boxes):
+  """
+  Determines if all the boxes can be opened.
+
+  Args:
+    boxes: A list of lists, where each inner list represents the keys to unlock
+      the other boxes.
+
+  Returns:
+    True if all boxes can be opened, else False.
+  """
+
+  # Initialize a set to track the boxes that have been unlocked.
+  unlocked = {0}
+
+  # Iterate over all the boxes.
+  for box in boxes:
+    # Add any unlocked boxes to the set.
+    for key in box:
+      if key in unlocked:
+        unlocked.add(key)
+
+  # Return True if all the boxes have been unlocked.
+  return len(unlocked) == len(boxes)
