adc35cd5f93c4ff87585be8e754c42d0650fd994
Noah Tsegay <noahtsegay8@gmail.com>
2023-07-06 23:07:08 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
+"""Lockboxes Contains method that finds the keys to
+open other lockboxes
+"""
+
 
 def canUnlockAll(boxes):
-  """
-  Determines if all the boxes can be opened.
+    """
+    Function that determines if you can open all the lockboxes
+    Args:
+        boxes: list of lists of integers
+    Returns:
+        True if you can open all the lockboxes, False otherwise
+    """
+    unlocked = set()
 
-  Args:
-    boxes: A list of lists, where each inner list represents the keys to unlock
-      the other boxes.
-
-  Returns:
-    True if all boxes can be opened, else False.
-  """
-
-  # Initialize a set to track the boxes that have been unlocked.
-  unlocked = {0}
-
-  # Iterate over all the boxes.
-  for box in boxes:
-    # Add any unlocked boxes to the set.
-    for key in box:
-      if key in unlocked:
-        unlocked.add(key)
-
-  # Return True if all the boxes have been unlocked.
-  return len(unlocked) == len(boxes)
+    for box_id, box in enumerate(boxes):
+        if len(box) == 0 or box_id == 0:
+            unlocked.add(box_id)
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                unlocked.add(key)
+        if len(unlocked) == len(boxes):
+            return True
+    return False
