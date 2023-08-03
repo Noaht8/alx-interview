f505a55597b5a3c5c7c8872a0ccb7682061bd5eb
Noah Tsegay <noahtsegay8@gmail.com>
2023-08-03 21:09:49 +0300

    Update 0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Solves N Queens Puzzle"""
+import sys
+
+
+def is_safe(board, row, col):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i] == col:
+            return False
+
+    # Check if there is a queen in the upper left diagonal
+    i = row - 1
+    j = col - 1
+    while i >= 0 and j >= 0:
+        if board[i] == j:
+            return False
+        i -= 1
+        j -= 1
+
+    # Check if there is a queen in the upper right diagonal
+    i = row - 1
+    j = col + 1
+    while i >= 0 and j < len(board):
+        if board[i] == j:
+            return False
+        i -= 1
+        j += 1
+
+    return True
+
+
+def solve_nqueens(n):
+    def backtrack(board, row):
+        if row == n:
+            solutions.append(board.copy())
+        else:
+            for col in range(n):
+                if is_safe(board, row, col):
+                    board[row] = col
+                    backtrack(board, row + 1)
+
+    solutions = []
+    board = [-1] * n
+    backtrack(board, 0)
+    return solutions
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
